957d35a19d2b202b1776235b69beba0c37a69559
n-inja <mccreepertakumi@gmail.com>
2016-11-02 23:35:27 +0900

    unity

---
+++ Assets/dokan.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dokan : MonoBehaviour {
+    public float speed = 1.0f;
+    public int ran = 114514;
+    System.Random r = new System.Random(1);
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Transform>().position = new Vector2(GetComponent<Transform>().position.x - speed, GetComponent<Transform>().position.y);
+        int a = r.Next(-2500, 2500);
+        if(GetComponent<Transform>().position.x < -11.0f)
+        {
+            float f = r.Next(-2500, 2500)/1000.0f;
+            GetComponent<Transform>().position = new Vector2(11.0f, f);
+        }
+	}
+}
---
+++ Assets/script.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class script : MonoBehaviour {
+    public float speed = 3.0f;
+    bool isJump = false;
+
+    // Use this for initialization
+    void Start () {
+        isJump = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isJump && Input.GetButton("Fire1"))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        isJump = false;
+        if(GetComponent<Rigidbody2D>().velocity.y > 0)
+        {
+            isJump = true;
+        }
+	}
+}
